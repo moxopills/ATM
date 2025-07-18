b088816819cda385dd34981983313fbf606be3b6
moxopills <moxopills@gmail.com>
2025-07-18 13:24:25 +0900

    ✨ 영수증 보기 기능 추가
    영수증 보기에 입금한 금액과 현재 잔액등 사용자가 내용을 확인가능 한 기능 추가

--- ATM.py
+++ ATM.py
@@ -1,4 +1,5 @@
 balance =  50000
+receipts = []
 
 while True:
     num=input("원하는 기능의 번호를 선택해주세요. 1.입금 2.출금 3.영수증 보기 4.종료: ")
@@ -9,12 +10,19 @@ while True:
         deposit_amount = int(input('입금할 금액을 입력해주세요: '))
         balance += deposit_amount #balunce = balunce + deposit_amount 랑 같음
         print(f'입금하신 금액은 {deposit_amount}원 이고 현재 잔액은 {balance}원 입니다. ')
+        receipts.append( ('입금', deposit_amount, balance) )
+
     if num == '2':
         withdrew_amount = int(input('출금할 금액을 입력해주세요. '))
         withdrew_amount = min(balance,withdrew_amount)
         balance -= withdrew_amount
         print(f'출금하신 금액은{withdrew_amount}원 이고 현재 잔액은 {balance}원 입니다. ') 
+        receipts.append(( '출금',withdrew_amount,balance ))
     if num == '3':
-        pass
-
+        if receipts:
+            for i in receipts:
+                print(f"{i[0]}: {i[1]}원 | 잔액: {i[2]}")
+        else:
+            print("영수증 내역이 없습니다. ")
+            
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.')
